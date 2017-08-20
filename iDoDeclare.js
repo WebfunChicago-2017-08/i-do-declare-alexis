bc840a489445afca9ed1fac7b1880e7a7a590c90
Alexis Gomez <gomez.alexis@gmail.com>
2017-08-20 16:00:35 -0500

    First work of JavaScript

---
+++ iDoDeclare.js
@@ -0,0 +1,16 @@
+var num1 = 13, num2 = 34.04, num3 = 1032, num4 = -234;
+var string1 = "I love my mom!";
+var string2 = "I love Monday's!", string3 = "333 W Wacker Dr, Chicago, IL";
+var boolean1 = false, boolean2 = true;
+var undefined1;
+
+console.log("The first number: " + num1);
+console.log("The second number: " + num2);
+console.log("The third number: " + num3);
+console.log("The fourth number: " + num4);
+console.log("The first String: " + string1);
+console.log("The second String: " + string2);
+console.log("The third String: " + string3);
+console.log("The first boolean: " + boolean1);
+console.log("The second boolean: " + boolean2);
+console.log("The first undefined: " + undefined1);
